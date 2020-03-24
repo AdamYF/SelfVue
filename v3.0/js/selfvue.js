1e3c801797bfc94667b6247736f23de1d5f5ac31
AdamYF <adam_yf@126.com>
2020-03-24 17:32:40 +0800

    add test for v3.0

---
+++ v3.0/js/selfvue.js
@@ -0,0 +1,10 @@
+function SelfVue(data, el, exp) {
+  this.data = data;
+  observer(data);
+  el.innerHTML = this.data[exp];
+  new Watcher(this, exp, function (value) {
+    el.innerHTML = value;
+  });
+  return this;
+}
+
